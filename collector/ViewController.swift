85469937fd3de2ecb611eddf86301324a9a471f3
Swuff <github@swuff.de>
2020-12-28 12:34:40 +0100

    Initial Commit

---
+++ collector/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  collector
+//
+//  Created by Sven Plaschke on 28.12.20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
